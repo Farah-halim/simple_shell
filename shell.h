5dd855b6dd31922deaca831cb043ab711dcd9bf9
Farah-halim <level2bis2@gmail.com>
2023-05-24 13:16:49 -0700

    update shell.h

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,9 +13,9 @@ int execute_command(char **args);
 char **take_input(char *buffer, size_t bufsize);
 extern char **environ;
 void _env(char **environ);
-ssize_t _getline(char** lineptr, size_t* n, FILE* stream);
-char* get_command_path(const char* command);
-ssize_t read_buffer(char* buffer, size_t* buffer_index,ssize_t* chars_read, FILE* stream);
-
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+char *get_command_path(const char *command);
+ssize_t read_buffer(char *buffer, size_t *buffer_index,
+		ssize_t *chars_read, FILE *stream);
 
 #endif
