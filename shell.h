4dd9286c390857541f5ed6614642ddc983b29f4a
Farah-halim <level2bis2@gmail.com>
2023-05-24 09:10:08 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int execute_command(char **args);
+char **take_input(char *buffer, size_t bufsize);
+extern char **environ;
+void _env(char **environ);
+ssize_t _getline(char** lineptr, size_t* n, FILE* stream);
+char* get_command_path(const char* command);
+ssize_t read_buffer(char* buffer, size_t* buffer_index,ssize_t* chars_read, FILE* stream);
+
+
+#endif
